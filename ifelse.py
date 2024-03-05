bf32f3e771b0ec265ecc7fe0f61b2ea36db0b189
Nitish <nandurinitish@gmail.com>
2024-03-05 17:21:00 +0530

    ifelse program

---
+++ ifelse.py
@@ -0,0 +1,15 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+if num1 > num2:
+    print("num1 is greater than num2")
+else:
+    print("num2 is greater than num1")
+
+
+#for loop
+
+obj = [1, 2, 3, 4, 5]
+for i in obj:
+    print(i)
+
